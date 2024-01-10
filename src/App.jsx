95e4e6adedb5ade5441cf2c16139f9220896eff4
Olena Mitropolska <olena.mitropolska@gmail.com>
2024-01-10 17:39:49 +0100

    router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components pages/Navbar';
+import Footer from './Components pages/Footer';
 
 function App() {
   return (
     <div data-theme="light" className="App">
       <Navbar />
+
+      <Routes>
+        <Route />
+      </Routes>
  <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+
+    <Footer />
     </div>
   );
 }
