96d7aec4c130effa927f9eb48a8b3406184f8367
Olena Mitropolska <olena.mitropolska@gmail.com>
2024-01-10 17:36:09 +0100

    navbar draft added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Navbar from './Components pages/Navbar';
 
 function App() {
   return (
     <div data-theme="light" className="App">
+      <Navbar />
  <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
