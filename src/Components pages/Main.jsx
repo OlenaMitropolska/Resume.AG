c95428178d3a38a59eb006f0253b525db56a5a86
Olena Mitropolska <olena.mitropolska@gmail.com>
2024-01-12 14:32:46 +0100

    main page main section done

--- src/Components pages/Main.jsx
+++ src/Components pages/Main.jsx
@@ -7,9 +7,9 @@ function Main() {
 
 <div className="hero h-screen mainhero" style={{backgroundImage: 'url(https://ik.imagekit.io/lnmtrplsk/IMG_5959.JPG?updatedAt=1704984883054)'}}>
   <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center herotext heroTextDiv">
-      <h1 className="mb-5 text-3xl font-bold">Alexander Gossmann</h1>
-      <p className="mb-5 text-xl">Consultant</p>
+  <div className="hero-content text-center flex flex-col relative left-72">
+      <h1 className="text-6xl font-bold text-white">Alexander Gossmann</h1>
+      <p className="text-2xl  italic text-white">Consultant</p>
   </div>
 </div>
         {/* className='mainPage'
