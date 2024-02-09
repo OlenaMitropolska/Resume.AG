6f482edd7a499762c04aca9f78d1c2bdf414b84e
Olena Mitropolska <olena.mitropolska@gmail.com>
2024-02-09 17:04:24 +0100

    contact page main design finished

--- src/Components pages/Contact.jsx
+++ src/Components pages/Contact.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+
+
+        <div className=" contact">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Kontaktieren mich!</h1>
+      <p className="py-6">Wenn Sie fragen haben oder ... bitte mich kontaktieren</p>
+    </div>
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form className="card-body">
+        <div className="form-control">
+            <span className="label-text">Email</span>
+            <p>ag.@gmail.com</p>
+        </div>
+        <hr />
+        <div className="form-control">
+            <span className="label-text">Nummer</span>
+            <p>491718279382</p>
+        </div>
+        
+      </form>
+    </div>
+  </div>
+</div>
+
+
+
+    </div>
   )
 }
 
